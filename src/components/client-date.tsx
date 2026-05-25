9be5f53a5c7176952654adbdb9e6939e18307701
Mariano Tucat <mtucat@gmail.com>
2026-05-25 09:43:25 -0300

    Update client-date.tsx

--- src/components/client-date.tsx
+++ src/components/client-date.tsx
@@ -1,15 +1,20 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+
 export function ClientDate({ iso }: { iso: string }) {
-  const date = new Date(iso)
-  return (
-    <span suppressHydrationWarning>
-      {date.toLocaleString('es-AR', {
+  const [text, setText] = useState('')
+
+  useEffect(() => {
+    setText(
+      new Date(iso).toLocaleString('es-AR', {
         day: '2-digit',
         month: '2-digit',
         hour: '2-digit',
         minute: '2-digit',
-      })}
-    </span>
-  )
+      })
+    )
+  }, [iso])
+
+  return <span>{text}</span>
 }
