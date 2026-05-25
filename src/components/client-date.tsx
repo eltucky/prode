370821ea43066b831c451f68f7ae8baa0941c41d
Mariano Tucat <mtucat@gmail.com>
2026-05-25 10:13:19 -0300

    trying to fix tz

--- src/components/client-date.tsx
+++ src/components/client-date.tsx
@@ -6,12 +6,14 @@ export function ClientDate({ iso }: { iso: string }) {
   const [text, setText] = useState('')
 
   useEffect(() => {
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
     setText(
       new Date(iso).toLocaleString('es-AR', {
         day: '2-digit',
         month: '2-digit',
         hour: '2-digit',
         minute: '2-digit',
+        timeZone,
       })
     )
   }, [iso])
